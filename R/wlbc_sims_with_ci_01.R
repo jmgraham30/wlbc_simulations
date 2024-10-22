c7dbbcebb13abc8e1f67c92e186b7da44b11b5c0
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-10-22 15:34:32 -0400

    update scripts for ci sims

--- R/wlbc_sims_with_ci_01.R
+++ R/wlbc_sims_with_ci_01.R
@@ -14,7 +14,7 @@ rep_num_vals <- 1:25 # number of replicates
 N_val_vals <- c(1e3,1e4,1e5,1e6) # population size
 F_m_vals <- seq(1.0,1.5,by=0.025) # fecundity 
 F_cv_vals <- c(0.0,0.01,0.1) # coefficient of variation for F
-sh_vals <- c(0.1,0.45) # CI strength
+sh_vals <- c(0.1) # CI strength
 # transmission rates and binomial proportions
 mu_vect_vals <- list(c(0.001,0.8),c(0.005,0.8),c(0.01,0.8),c(0.05,0.8),
                      c(0.1,0.8),c(0.15,0.8),c(0.2,0.8),c(0.25,0.8),
@@ -40,4 +40,4 @@ sim_with_ci_df <- future_pmap(parms_df, infection_freq_rf_rmu_ci_sim,
 # save the results, note that only every other row is required
 # save the results, note that only every other row is required
 saveRDS(sim_with_ci_df[seq(1,nrow(sim_with_ci_df),by=2), ],
-        file = "mu_sims_data/with_ci_sims.rds")
+        file = "mu_sims_data/with_ci_sims_01.rds")
