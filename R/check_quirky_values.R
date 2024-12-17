9ecae63fe5ef1e508118e0a968a626febe47e5b2
Jason M Graham <prof.jason.m.graham@gmail.com>
2024-12-17 09:03:48 -0500

    update

--- R/check_quirky_values.R
+++ R/check_quirky_values.R
@@ -7,14 +7,26 @@ sim_res <- read_csv("mu_sims_data/wlbc_simulations_results.csv")
 glimpse(sim_res)
 
 
-sim_res <- sim_res |>
-  filter(bin_props < 1.0,F_val_m > 1) |>
+sim_res_1 <- sim_res |>
+  filter(s_h == 0, bin_props < 1, F_val_m > 1) |>
   mutate(bin_props_c = 1 - bin_props,
          mu_vect_c = 0.8,
          p_star = 1 - (F_val_m/(F_val_m - 1))*(mu_vect*bin_props + mu_vect_c*bin_props_c))
 
+glimpse(sim_res_1)
+
+sim_res_2 <- sim_res |>
+  filter(s_h > 0, bin_props < 1, F_val_m > 1) |>
+  mutate(bin_props_c = 1 - bin_props,
+         mu_vect_c = 0.8,
+         p_star = (s_h+1-F_val_m - sqrt((s_h+1-F_val_m)^2 - 4*s_h*(F_val_m*(1 - mu_vect*bin_props + mu_vect_c*bin_props_c) - 1)*(1 - F_val_m*(mu_vect*bin_props + mu_vect_c*bin_props_c))))/(2*s_h*(1 - F_val_m*(mu_vect*bin_props + mu_vect_c*bin_props_c))))
+
+glimpse(sim_res_2)
+
+sim_res <- bind_rows(sim_res_1, sim_res_2)
+
 sim_res <- sim_res |>
-  mutate(p_star_neg = ifelse(p_star < 0, "Yes", "No"))
+  mutate(p_star_neg = ifelse(p_star < 0 | is.nan(p_star), "Yes", "No"))
 
 glimpse(sim_res)
 
