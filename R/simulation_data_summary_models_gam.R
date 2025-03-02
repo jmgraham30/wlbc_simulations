99e1a7957af916989ae93cc3eb94869c8e9c4e34
Jason M Graham <prof.jason.m.graham@gmail.com>
2025-03-02 11:57:50 -0500

    update

--- R/simulation_data_summary_models_gam.R
+++ R/simulation_data_summary_models_gam.R
@@ -47,27 +47,24 @@ p_t_sd_model_l <- bam(p_t_sd ~ N_val + s_h_fct +
 plot_predictions(p_t_mean_model_l,condition = c("mu_vect","F_val","s_h_fct","mu_groups_fct"))
 plot_predictions(p_t_sd_model_l,condition = c("mu_vect","F_val","s_h_fct","mu_groups_fct"))
 
-p_t_mean_preds <- plot_predictions(p_t_mean_model_l,
-                                   condition = c("mu_vect","F_val","s_h_fct","mu_groups_fct"),
-                                   draw = FALSE)
+p_t_mean_preds <- predictions(p_t_mean_model_l,
+                                newdata=datagrid(mu_vect = unique(data_df$mu_vect),
+                                                 F_val = c(0.0,0.1,0.2,0.3,0.4,0.5),
+                                                 mu_groups_fct = unique(data_df$mu_groups_fct),
+                                                 s_h_fct = unique(data_df$s_h_fct),
+                                                 F_cv_fct = unique(data_df$F_cv_fct))) |>
+  as.data.frame()
 
-p_t_sd_preds <- plot_predictions(p_t_sd_model_l,
-                                   condition = c("mu_vect","F_val","s_h_fct","mu_groups_fct"),
-                                   draw = FALSE)
+p_t_sd_preds <- predictions(p_t_sd_model_l,
+                              newdata=datagrid(mu_vect = unique(data_df$mu_vect),
+                                               F_val = c(0.0,0.1,0.2,0.3,0.4,0.5),
+                                               mu_groups_fct = unique(data_df$mu_groups_fct),
+                                               s_h_fct = unique(data_df$s_h_fct),
+                                               F_cv_fct = unique(data_df$F_cv_fct))) |>
+  as.data.frame()
 
-p_t_mean_preds <- p_t_mean_preds |>
-  mutate(F_val = fct_recode(F_val, "0" = "0.00",
-                              "0.2" = "0.20",
-                              "0.3" = "0.32",
-                              "0.4" = "0.43",
-                              "0.5" = "0.50"))
-
-p_t_sd_preds <- p_t_sd_preds |>
-  mutate(F_val = fct_recode(F_val, "0" = "0.00",
-                              "0.2" = "0.20",
-                              "0.3" = "0.32",
-                              "0.4" = "0.43",
-                              "0.5" = "0.50"))
+glimpse(p_t_mean_preds)
+glimpse(p_t_sd_preds)
 
 write_csv(p_t_mean_preds,"mu_sims_data/p_t_mean_preds.csv")
 write_csv(p_t_sd_preds,"mu_sims_data/p_t_sd_preds.csv")
