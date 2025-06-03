2618275ad151fc4e3b3f800c366ade381f98fc67
Jason M Graham <prof.jason.m.graham@gmail.com>
2025-06-03 08:31:17 -0400

    update

---
+++ R/ten_to_four_separate_F.R
@@ -0,0 +1,30 @@
+# load packages
+library(tidyverse) # for plots and data manipulation
+library(latex2exp) # for LaTeX expressions
+library(scales) # for scales
+library(forcats) # for factor manipulation
+library(wlbcmodeler) # for wolbachia model functions
+
+# set plot theme
+theme_set(theme_bw())
+
+# load data
+plot_df <- read_csv("mu_sims_data/wlbc_simulations_results.csv")
+
+glimpse(plot_df)
+
+# filter 10^4
+plot_df_ten_four <- plot_df |>
+  filter(N_val == 1e+04) 
+
+glimpse(plot_df_ten_four)
+
+plot_df_ten_four |>
+  ggplot(aes(x = p_t_std, y = p_t_mean)) + 
+  geom_point(alpha=0.25) +
+  facet_wrap(~ F_cv_fct, scales = "free_x") +
+  labs(
+    x = TeX("$p_t$ (standard deviation)"),
+    y = TeX("$p_t$ (mean)"),
+    title = TeX("$10^4$ simulations: $F$ coefficient of variation")
+  ) 
