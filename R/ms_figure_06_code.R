d5cf9c06bbe4d81faaf63f1f2aa58b1133e2efcb
Jason M Graham <prof.jason.m.graham@gmail.com>
2025-06-12 09:57:25 -0400

    update figure file names

--- R/ms_figure_06_code.R
+++ R/ms_figure_06_code.R
@@ -3,74 +3,74 @@ library(tidyverse) # for plots and data manipulation
 library(latex2exp) # for LaTeX expressions
 library(scales) # for scales
 library(forcats) # for factor manipulation
-library(ggthemes) # for color theme
-library(patchwork) # for combining plots
 
 # set plot theme
 theme_set(theme_bw())
 
 # load data
-plot_05_mu_df <- read_csv("mu_sims_data/wlbc_simulations_results.csv") 
+plot_02_mu_df <- read_csv("mu_sims_data/wlbc_simulations_results.csv")
+
+glimpse(plot_02_mu_df)
+
+plot_02_mu_df <- plot_02_mu_df |>
+  filter(N_val == 10000, s_h != 0.45, F_cv == 0.0,p_t_var > 0) |>
+  mutate(mu_groups_fct = factor(mu_groups_fct)) |>
+  mutate(mu_group = fct_relevel(mu_groups_fct, c("mu_group = 1.0", "mu_group = 0.9")))
 
 # glimpse data
-glimpse(plot_05_mu_df)
+glimpse(plot_02_mu_df)
 
-plot_05_mu_df <- plot_05_mu_df |>
-  filter(s_h != 0.45, p_t_var > 0.0) |>
-  mutate(p_t_range = p_t_max - p_t_min) |>
-  mutate(mu_group = factor(mu_groups_fct)) |>
-  mutate(mu_group = fct_relevel(mu_group, c("mu_group = 1.0", "mu_group = 0.9"))) |>
-  mutate(F_cv_group = factor(F_cv_fct)) |>
-  mutate(F_cv_group = fct_relevel(F_cv_group, c("F_cv = 0.0", "F_cv = 0.01", "F_cv = 0.1")))
+############### CREATE PLOTS FOR FIGURE 2 ################
 
-# glimpse data
-glimpse(plot_05_mu_df)
-
+# breaks for the color scale
 mybreaks_freq <- seq(0, 1, 0.15)
+mybreaks_std <- round(log10(c(0.00004,0.00008,0.0001,0.0005,0.001,0.005,0.007,0.009)),3)
 
-### plot ###
-p_0 <- plot_05_mu_df |>
-  filter(s_h_fct == "s_h = 0.0") |>
-  ggplot(aes(x = mu_group, y = p_t_range, fill = F_cv_group)) +
-  geom_boxplot(outliers=FALSE) + 
-  geom_point(aes(color = p_t_mean), 
-             position = position_jitterdodge(jitter.width=0.15),size=0.45) + 
-  facet_wrap(~N_val, nrow=1) +
-  scale_fill_colorblind() +
-  #scale_color_gradient(low = "lightgreen", high = "darkgreen") +
-  scale_color_gradientn(
+
+plot_02_mu_df |>
+  group_by(F_val_m,mu_vect,s_h_fct,mu_group) |>
+  summarise(p_t_mean = mean(p_t_mean)) |>
+  ggplot(aes(y = F_val_m, x = as.character(mu_vect), fill = p_t_mean)) +
+  geom_tile() +
+  scale_fill_gradientn(
     colours = hcl.colors(length(mybreaks_freq), "viridis", rev = FALSE), 
     breaks = mybreaks_freq
   ) +
-  labs(
-    x = TeX("$\\mu$ group"), 
-    y = "Frequency range", 
-    color = "Mean \n frequency", 
-    fill = "CV",
-    title = TeX("$s_h = 0.0$")
-  ) + ylim(c(0, 1))
-
-p_1 <- plot_05_mu_df |>
-  filter(s_h_fct == "s_h = 0.1") |>
-  ggplot(aes(x = mu_group, y = p_t_range, fill = F_cv_group)) +
-  geom_boxplot(outliers = FALSE) + 
-  geom_point(aes(color = p_t_mean), 
-             position = position_jitterdodge(jitter.width=0.15),size=0.45) + 
-  facet_wrap(~N_val, nrow=1) +
-  scale_fill_colorblind() +
-  #scale_color_gradient(low = "lightgreen", high = "darkgreen") +
-  scale_color_gradientn(
-    colours = hcl.colors(length(mybreaks_freq), "viridis", rev = FALSE), 
-    breaks = mybreaks_freq
+  facet_wrap(~ s_h_fct + mu_group) +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
   ) +
+  scale_y_continuous(expand = c(0, 0)) +
   labs(
-    x = TeX("$\\mu$ group"), 
-    y = "Frequency range", 
-    color = "Mean \n frequency", 
-    fill = "CV",
-    title = TeX("$s_h = 0.1$")
-  ) + ylim(c(0, 1))
+    x = TeX("$\\mu$"),
+    y = TeX("$F$"),
+    fill = TeX("Mean \n frequency"),
+  )
 
-
-p_0 / p_1
+plot_02_mu_df |>
+  mutate(p_t_std_log = log10(sqrt(p_t_var))) |>
+  group_by(F_val_m,mu_vect,s_h_fct,mu_group) |>
+  summarise(p_t_std_log = mean(p_t_std_log)) |>
+  ggplot(aes(y = F_val_m, x = as.character(mu_vect), fill = p_t_std_log)) +
+  geom_tile(stat="identity") +
+  scale_fill_gradientn(
+    colours = hcl.colors(length(mybreaks_std), "viridis", rev = FALSE), 
+    breaks = mybreaks_std
+  ) +
+  facet_wrap(~ s_h_fct + mu_group) +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    legend.text = element_text(size = 12),
+    legend.title = element_text(size = 14),
+  ) +
+  scale_y_continuous(expand = c(0, 0)) +
+  labs(
+    x = TeX("$\\mu$"),
+    y = TeX("$F$"),
+    fill = TeX("Frequency \n std (log)"),
+  )
 
